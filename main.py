7564178d4eeb30af1ca396154c2235fc19c505fe
Your Name <you@example.com>
2026-04-30 15:04:58 -0500

    Actualizar configuración de CORS para permitir solo el origen "1"

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["1"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
