46da30669a0addb1b630fd67d4318c7f2a5cf291
Am30za <damartineza187@gmail.com>
2026-04-30 23:35:58 -0500

    limpiar indice de archivos no deseados

--- university-2/models/student_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Student(BaseModel):
-    name: str = Field(..., min_length=2)
-    age: int = Field(...,gt=0)
-    grade: float=Field(...,ge=0, le=5)
-
-class StudentResponse(Student):
-    id: int
